75bbefe8026e47b06923b09bc4e5ec2d37938791
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-05-15 10:22:46 +0200

    V2 - Stage and Bitmap

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 (function() {
-
+    
+    var stage;
+    
     window.initialize = function() {
+        
+        // Create new stage on the canvas
+        stage = new createjs.Stage(document.getElementById("match_3"));
+        
+        // Create a new Bitmap Object
+        var bitmap = new createjs.Bitmap("assets/blueDude.png");
+        
+        // Add it to stage
+        stage.addChild(bitmap);
+        
+        // Update the stage to display the Bitmap
+        stage.update();
         
     };
 
