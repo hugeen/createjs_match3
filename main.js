dd00b24a57bf0f942a3272ba9b54af7396d51ac1
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-05-15 10:44:11 +0200

    V4 Gem initialization

--- main.js
+++ main.js
@@ -3,30 +3,37 @@
     // Game class
     function Game(stage) {
         
+        this.stage = stage;
+        
         // Create a new jMatch3 Grid
         this.grid = new jMatch3.Grid({
             width: 6,
             height: 7,
             gravity: "down"
         });
+
+    }
+    
+    // Gem class
+    function Gem(game, type) {
+        this.game = game; // game reference
+        this.type = type;
         
+        // Create new bitmap
+        this.bitmap = new createjs.Bitmap("assets/"+type+".png");
+        
+        // Add it to game stage
+        this.game.stage.addChild(this.bitmap);
     }
 
     window.initialize = function() {
-        
+
         // Create new stage on the canvas
         var stage = new createjs.Stage(document.getElementById("match_3"));
         var game = new Game(stage);
-        
-        // Create a new Bitmap Object
-        var bitmap = new createjs.Bitmap("assets/blueDude.png");
-        
-        // Add it to stage
-        stage.addChild(bitmap);
-        
-        // Update the stage to display the Bitmap
+        new Gem(game, "blueDude");
         stage.update();
-        
+
     };
 
 })();
