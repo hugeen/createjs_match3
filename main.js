1461232c6cc9edc05747bf6b9639d0e662a5eb6b
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-05-15 10:57:03 +0200

    V4.5 Move Gem

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 (function() {
-
+    
+    // Gem size constant in pixels
+    var GEMSIZE = 48;
+    
     // Game class
     function Game(stage) {
         
@@ -15,23 +18,33 @@
     }
     
     // Gem class
-    function Gem(game, type) {
+    function Gem(game, type, x, y) {
         this.game = game; // game reference
         this.type = type;
         
         // Create new bitmap
         this.bitmap = new createjs.Bitmap("assets/"+type+".png");
         
+        this.move(x, y);
+        
         // Add it to game stage
         this.game.stage.addChild(this.bitmap);
     }
+    
+    // Move method
+    Gem.prototype.move = function(x, y) {
+        this.x = x;
+        this.y = y;
+        this.bitmap.x = this.x * GEMSIZE;
+        this.bitmap.y = this.y * GEMSIZE;
+    };
 
     window.initialize = function() {
 
         // Create new stage on the canvas
         var stage = new createjs.Stage(document.getElementById("match_3"));
         var game = new Game(stage);
-        new Gem(game, "blueDude");
+        new Gem(game, "blueDude", 2, 0);
         stage.update();
 
     };
