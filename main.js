39aee56dbbf3a858041b133f5606dc2789e2f29c
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-05-15 12:04:54 +0200

    V8 - Drop

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
     var MARGINTOP = 2;
 
     // Game class
+
+
     function Game(stage) {
 
         this.stage = stage;
@@ -20,35 +22,36 @@
 
         // Create new Gem
         var currentGem;
-        
+
         this.newGem = function() {
             currentGem = new Gem(this, "blueDude", 0, 0);
         };
-        
+
         this.newGem();
-        
+
         // Bind a function to each keys
         var keys = {
             left: function() {
                 if (currentGem) {
-                    currentGem.move(currentGem.x-1, currentGem.y);
+                    currentGem.move(currentGem.x - 1, currentGem.y);
                 }
             },
             right: function() {
                 if (currentGem) {
-                    currentGem.move(currentGem.x+1, currentGem.y);
+                    currentGem.move(currentGem.x + 1, currentGem.y);
                 }
             },
             up: function() {
-                
+
             },
             down: function() {
                 if (currentGem) {
-                    currentGem.fall(currentGem.x, 6);
+                    currentGem.drop();
+                    currentGem = false;
                 }
             }
         };
-        
+
         // Handle keys
         this.handleKeyPressed = function(key) {
             keys[key]();
@@ -58,6 +61,8 @@
     }
 
     // Gem class
+
+
     function Gem(game, type, x, y) {
         this.game = game; // game reference
         this.type = type;
@@ -79,17 +84,38 @@
         this.bitmap.y = this.y * GEMSIZE;
     };
 
+    // Drop method
+    Gem.prototype.drop = function() {
+        
+        // Get the gem column
+        var column = this.game.grid.getColumn(this.x);
+        
+        // Get the last empty piece to place the gem
+        var lastEmpty = jMatch3.Grid.getLastEmptyPiece(column);
+
+        // If an empty piece has been found
+        if (lastEmpty) {
+            
+            // Bind this gem to the piece
+            lastEmpty.object = this;
+            
+            // And make it fall
+            this.fall(lastEmpty.x, lastEmpty.y);
+        }
+    }
+
     // Fall method
     Gem.prototype.fall = function(x, y) {
-        
+
         var gem = this;
-        
+
         // Create a tween animation
         createjs.Tween.get(this.bitmap).to({
             x: x * GEMSIZE,
             // End x position
             y: MARGINTOP * GEMSIZE + y * GEMSIZE // End y position
         }, 500, createjs.Ease.cubicOut).call(function() {
+            // Create new gem
             gem.game.newGem();
         });
 
