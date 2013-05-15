4f0d938b920793c036b7f6f59538959b13c4c4f0
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-05-15 11:25:57 +0200

    V5 - Fall

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
     // Gem size constant in pixels
     var GEMSIZE = 48;
     
+    // Margin top equal to 2 gems height
+    var MARGINTOP = 2;
+    
     // Game class
     function Game(stage) {
         
@@ -38,13 +41,31 @@
         this.bitmap.x = this.x * GEMSIZE;
         this.bitmap.y = this.y * GEMSIZE;
     };
-
+    
+    // Fall method
+    Gem.prototype.fall = function(x, y) {
+        
+        // Create a tween animation
+        createjs.Tween.get(this.bitmap).to({
+            x: x * GEMSIZE, // End x position
+            y: MARGINTOP * GEMSIZE + y * GEMSIZE // End y position
+        }, 500, createjs.Ease.cubicOut);
+        
+    };
+    
     window.initialize = function() {
 
         // Create new stage on the canvas
         var stage = new createjs.Stage(document.getElementById("match_3"));
+        
+        // Create a ticker
+        createjs.Ticker.addEventListener("tick", function(event) {
+            stage.update(event);
+        });
+        
         var game = new Game(stage);
-        new Gem(game, "blueDude", 2, 0);
+        var gem = new Gem(game, "blueDude", 2, 0);
+        gem.fall(2, 6);
         stage.update();
 
     };
